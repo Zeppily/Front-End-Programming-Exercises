ed9c40fd0978c2fb331a576780db26382f348679
Zeppily <thaigrrs@gmail.com>
2020-02-06 13:25:54 +0200

    + todolist with functional date input, renaming files

--- ToDoList/thielman-react/src/components/Todolist.js
+++ ToDoList/thielman-react/src/components/Todolist.js
@@ -1,28 +1,41 @@
 import React, {useState,useEffect} from 'react';
 
 export default function Todolist(){
-    const[desc, setDesc] = useState('');
+    const[todo, setTodo] = useState({desc: '', date: ''});
     const[todos, setTodos] = useState([]);
 
     const inputChanged = (event) => {
-        setDesc(event.target.value);
+        setTodo({...todo, [event.target.name]: event.target.value});
+        /*spread notation, name linked to <input> 's name attribute */
     }
     
     const addTodo = (event) => {
-        event.preventDefault();
-        setTodos([...todos, desc]);
+        setTodos([{desc: todo.desc, date: todo.date}, ...todos]);
+        /*Creates objects with the current todo values*/ 
     }
 
     return(
     <div>
-        <input type="text" onChange={inputChanged} value={desc}/>
+        <label>Date: </label>
+        <input type="date" name="date" onChange={inputChanged} value={todo.date} />
+
+        <label>Description: </label>
+        <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
+        
         <button onClick={addTodo} >Add</button>
         <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
             <tbody>
             {
                 todos.map(   todo=> 
                     <tr>
-                        <td>{todo}</td>
+                        <td>{todo.date}</td>
+                        <td>{todo.desc}</td>
                     </tr>
                 )
             }
